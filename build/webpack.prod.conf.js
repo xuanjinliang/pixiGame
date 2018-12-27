a309674da8127c1ab307379e5ae31e1d14116aca
xuanjinliang <jinliang.xuan@mobvista.com>
2018-12-27 11:45:14 +0800

    添加gzip功能

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -90,7 +90,7 @@ if (config.build.productionGzip) {
 
   webpackConfig.plugins.push(
     new CompressionWebpackPlugin({
-      asset: '[path].gz[query]',
+      filename: '[path].gz[query]',
       algorithm: 'gzip',
       test: new RegExp(`\\.(${config.build.productionGzipExtensions.join('|')})$`),
       threshold: 10240,
